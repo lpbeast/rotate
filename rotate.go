8091ef627476c641e10c1294697725af28ec30ea
lpbeast <106935877+lpbeast@users.noreply.github.com>
2022-10-08 19:42:08 -0500

    ROT13 utility for practice.

--- rotate.go
+++ rotate.go
@@ -25,16 +25,73 @@ func rotate(i rune) rune {
 	return i
 }
 
-func main() {
+func strprocess(s string) string {
 	var rotrunes []rune
-	fmt.Println("Enter text to rotate.")
-	scanner := bufio.NewScanner(os.Stdin)
-    scanner.Scan()
-	clearrunes := []rune(scanner.Text())
+	clearrunes := []rune(s)
 	for _, v := range clearrunes {
 		u := rotate(v)
 		rotrunes = append(rotrunes, u)
 	}
-	rottext := string(rotrunes)
-	fmt.Println(rottext)
+	return string(rotrunes)
+}
+
+func main() {
+	argcount := len(os.Args[1:])
+	switch {
+	case argcount == 0:
+		//Accept a line of text from console, rotate it, display output on console
+		fmt.Println("Enter text to rotate.")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		rottext := strprocess(scanner.Text())
+		fmt.Println(rottext)
+		
+	case argcount == 1:
+		//Read the given file, rotate it, display output on console
+		fmt.Println("Input file is", os.Args[1])
+		
+		infile, err := os.Open(os.Args[1])
+		defer infile.Close()
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("Exiting.")
+			return
+		}
+
+		scanner := bufio.NewScanner(infile)
+		for scanner.Scan() {
+			rottext := strprocess(scanner.Text())
+			fmt.Println(rottext)
+		}
+		
+	case argcount == 2:
+		//Read the first file named, rotate it, write output to the second file named.
+		fmt.Println("Input file is", os.Args[1])
+		fmt.Println("Output file is", os.Args[2])
+		
+		infile, err := os.Open(os.Args[1])
+		defer infile.Close()
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("Exiting.")
+			return
+		}
+		
+		outfile, err := os.Create(os.Args[2])
+		defer outfile.Close()
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("Exiting.")
+			return
+		}
+		
+		scanner := bufio.NewScanner(infile)
+		for scanner.Scan() {
+			rottext := strprocess(scanner.Text()) + "\n"
+			outfile.WriteString(rottext)
+		}
+		
+	default:
+		fmt.Println("Too many arguments.")
+	}
 }
